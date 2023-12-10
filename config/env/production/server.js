9e34f444e23bd654d471ed63360771102c807638
PituPitu0 <PituPitu0@users.noreply.github.com>
2023-12-11 00:26:05 +0100

    11.12.2023.02

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  url: env("RENDER_EXTERNAL_URL"),
+  url: env("RENDER_EXTERNAL_URL", "http://localhost:1337"), // Domyślnie ustawione na lokalne środowisko
   dirs: {
-    public: "/data/public"
+    public: "/data/public",
   },
 });
